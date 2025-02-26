e2cb13836adf65de341ce1757fd4a7bdd59e6205
flavius <flaviuscojocaru19@gmail.com>
2025-02-26 12:53:32 +0200

    setting up db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <h1>Craiova App</h1>
-      <Image
-        src="/assets/olguta.jpg"
-        alt="craiova"
-        width={500}
-        height={500}
-      />
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.name}</h2>
+        </div>
+      ))}
+      <Image src="/assets/olguta.jpg" alt="craiova" width={500} height={500} />
     </main>
   );
 }
